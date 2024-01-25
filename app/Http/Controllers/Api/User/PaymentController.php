a5cfa6673089d37d5a67a1d76c5cd83aa533bd8c
eldapour <abdallahmahmoud1422003@gmail.com>
2024-01-25 11:57:12 +0200

    error logout 25_1_2024

--- app/Http/Controllers/Api/User/PaymentController.php
+++ app/Http/Controllers/Api/User/PaymentController.php
@@ -14,29 +14,7 @@ class PaymentController extends Controller
      */
     public function payment_verify(Request $request)
     {
-        // handle requests
-        $amount = $request->amount ?? 200;
-        $user_id = $request->user_id ?? 1;
-        $user_first_name = $request->user_first_name ?? 'test';
-        $user_last_name = $request->user_last_name ?? 'test';
-        $user_email = $request->user_email ?? 'test@test.com';
-        $user_phone = $request->user_phone ?? '01122717960';
-        $source = $request->source ?? 'test.com';
+       // handle payment
 
-        $payment = new PaymobPayment();
-
-        //pay function
-        $payment->pay(
-            $amount,
-            $user_id,
-            $user_first_name,
-            $user_last_name,
-            $user_email,
-            $user_phone,
-            $source
-        );
-        //verify function
-        $payment->verify($request);
-
-    }
-}
+    } // end Payment verify method
+} // Payment Controller
