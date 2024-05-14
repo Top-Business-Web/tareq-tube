55003db9bbd7387c49871f1c52bf5ae030962136
eldapour <abdallahmahmoud1422003@gmail.com>
2024-05-14 11:10:13 +0300

    14-5-2024

--- app/Repository/Api/User/UserRepository.php
+++ app/Repository/Api/User/UserRepository.php
@@ -42,7 +42,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Pagination\Paginator;
 
 class UserRepository extends ResponseApi implements UserRepositoryInterface
 {
