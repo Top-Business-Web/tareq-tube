61dac22d135d3f9f4959907b640c9b4802a1c8de
eldapour <abdallahmahmoud1422003@gmail.com>
2024-05-14 10:25:43 +0300

    14-5-2024

--- app/Repository/Api/User/UserRepository.php
+++ app/Repository/Api/User/UserRepository.php
@@ -898,8 +898,7 @@ class UserRepository extends ResponseApi implements UserRepositoryInterface
             if ($videos->count() > 0) {
 //                $randomVideo = $videos->random();
 //                dd($videos);
-                return $videos;
-//                return self::returnResponseDataApi($videos, 'تم الحصول على البيانات بنجاح', 200);
+                return self::returnResponseDataApi($videos, 'تم الحصول على البيانات بنجاح', 200);
             } else {
                 return self::returnResponseDataApi(null, 'لا يوجد بيانات', 200);
             }
